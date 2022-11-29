19b24f11aa4569680f8dd4033b33cb7964e0ec0a
Aric-W <u0586534@utah.edu>
2022-11-28 18:23:37 -0700

    got the linear  linear regression and plotting code to work for one line

--- runcode.py
+++ runcode.py
@@ -1,8 +1,14 @@
 import Hailstorm as H
 import DMARCGMforfinal as DMAR
 import LineOfBestFitGenerator as LN
+import numpy as np
 
 ints = H.FillInts(50)
 
+storm = H.Hailstorm(ints)
+x = np.arange(0,50,1)
+y1 = LN.GenLeastSquaresL(ints,storm,x)
 
+LN.PlotEverything(ints,storm,[y1],['red'],x)
 
+#print(LN.ProdSum([2,3],[4,5],1,1))
